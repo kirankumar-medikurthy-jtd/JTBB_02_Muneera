def92e31ebdf4c4e7961df6ce52aa6588c091b18
smuneera253 <119492034+smuneera253@users.noreply.github.com>
2023-04-08 06:59:06 +0530

    done
    done

---
+++ week_01/day_3/leapyear2.java
@@ -0,0 +1,15 @@
+class main
+{
+	public static void main(String[]args)
+	{
+		int  num=2004;
+			
+			if(num%4==0)
+			{
+				System.out.println("leap year");
+			}else
+			{
+				System.out.println("not a leap year");
+			}
+	}
+}
